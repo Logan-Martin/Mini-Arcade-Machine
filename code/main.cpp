b48ff99cb05e70db34066b521d0edbdad4115891
Logan Martin <71342159+Logan-Martin@users.noreply.github.com>
2026-08-21 15:41:25 -0400

    Updated main.cpp
    Added movement logic and persistent Score: 0. Doesn't change cause I'd need to make changes not expensive/per frame, and fix debounce things with buttons or figure it out, and before that screen tearing/updating glitching like movement is right now

--- code/main.cpp
+++ code/main.cpp
@@ -73,12 +73,13 @@ struct JoystickStruct {
   int xVal;
   int yVal;
   //
-  int MAX_read_val = 1008;
-  int MIN_read_val = 70;
-  int CENTER_REST_READ_X_VAL = 523;
-  int CENTER_REST_READ_Y_VAL = 538;
-  float deadzone = 1.15;
-
+  int MAX_TOTAL_read_val = 1008;
+  int MIN_TOTAL_read_val = 70;
+  int CENTER_REST_READ_VAL = 504;
+  float deadzone = 1.29;
+  
+  int UP_Req_Read_For_Movement;
+  int DOWN_Req_Read_For_Movement;
   //
 
   int buttonPin = 48;
@@ -141,6 +142,9 @@ void setup() {
   randomSeed(analogRead(0));
 
   // FOR JOYSTICK:
+  joystick.UP_Req_Read_For_Movement = joystick.deadzone * joystick.CENTER_REST_READ_VAL;
+  joystick.DOWN_Req_Read_For_Movement = (joystick.CENTER_REST_READ_VAL - (joystick.UP_Req_Read_For_Movement - joystick.CENTER_REST_READ_VAL));
+  //
   pinMode(joystick.xPin, INPUT);
   pinMode(joystick.yPin, INPUT);
   pinMode(joystick.buttonPin , INPUT_PULLUP);
@@ -157,11 +161,6 @@ void setup() {
   tft.fillScreen(BLUE);
 
   //tft.fillTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t color)
-
-  //tft.setTextColor(BLACK);
-
-  tft.setTextSize(3);
-  printWrappedText("Score: 0", ((SCREEN_WIDTH / SCREEN_WIDTH) + 5) , ((SCREEN_HEIGHT / SCREEN_HEIGHT) + 5) , 16, 1);
 
   Player.xPos = (SCREEN_WIDTH / 2);
   Player.yPos = (SCREEN_HEIGHT / 2);
@@ -186,31 +185,56 @@ void Input() { // reads player inputs
 }
 
 void Update(float deltaTime) { // Move things [plr, enemy, etc] w/ math, enemy AI, or calculate physics
-  bool xCondition  = (joystick.xVal >= (joystick.CENTER_REST_READ_X_VAL * -joystick.deadzone));
-  bool yCondition  = (joystick.yVal >= (joystick.CENTER_REST_READ_Y_VAL * -joystick.deadzone));
-  if (abs(xCondition_LOW) && yCondition_HIGH) { // if past deadzone
+  bool xCondition_RIGHT  = (joystick.xVal >= joystick.UP_Req_Read_For_Movement); // X Right Cond.
+  bool yCondition_UP  = (joystick.yVal >= joystick.UP_Req_Read_For_Movement); // Y Up Cond.
+  bool xCondition_LEFT  = (joystick.xVal <= joystick.DOWN_Req_Read_For_Movement); // X LEFT Cond.
+  bool yCondition_DOWN  = (joystick.yVal <= joystick.DOWN_Req_Read_For_Movement); // Y DOWN Cond.
+
+  Serial.print("DEADZONE: ");
+  Serial.print(joystick.CENTER_REST_READ_VAL * joystick.deadzone);
+  Serial.print("| X Read: ");
+  Serial.print(joystick.xVal);
+  Serial.print("| Y Read: ");
+  Serial.print(joystick.yVal);
+  Serial.println();
+
+  if (xCondition_RIGHT || xCondition_LEFT || yCondition_UP || yCondition_DOWN ) { // if past deadzone
     // UP -> Y up, Down -> Y down
     // Left -> X up, Right -> X down
-    float totalRange = (float)(joystick.MAX_read_val - joystick.MIN_read_val);
-    float xDirection = (((float)joystick.xVal - joystick.MIN_read_val ) / (totalRange / 2.0f)) - 1.0f;
-    float yDirection = (((float)joystick.yVal - joystick.MIN_read_val) / (totalRange / 2.0f)) - 1.0f;
+    float totalRange = (float)(joystick.MAX_TOTAL_read_val - joystick.MIN_TOTAL_read_val);
+    float xDirection = (((float)joystick.xVal - joystick.MIN_TOTAL_read_val ) / (totalRange / 2.0f)) - 1.0f;
+    float yDirection = (((float)joystick.yVal - joystick.MIN_TOTAL_read_val) / (totalRange / 2.0f)) - 1.0f;
 
-    Player.xPos += (xDirection * Player.walkSpeed * deltaTime);
-    Player.yPos += (yDirection * Player.walkSpeed * deltaTime);
+    Player.xPos += (-xDirection * Player.walkSpeed * deltaTime);
+    Player.yPos += (-yDirection * Player.walkSpeed * deltaTime);
   }
+  // Printing:
+  //Serial.print("X: ");
+  //Serial.print(joystick.xVal);
+  //Serial.print("| Y: ");
+  //Serial.print(joystick.yVal);
+  //Serial.println();
 }
 
 void Render(float deltaTime) { // draw the graphics based on the Update changes
-  // Player position updates: //
+  // get rid of old plr spot://
+  tft.fillCircle(PlayerCircle.xPos, PlayerCircle.yPos, PlayerCircle.radius , BLUE);
+  // Updated plr position:
   PlayerCircle.xPos = Player.xPos;
   PlayerCircle.yPos = Player.yPos;
   tft.fillCircle(PlayerCircle.xPos, PlayerCircle.yPos, PlayerCircle.radius , PlayerCircle.color);
 
+  // Probably want to render things in different steps.
+  // Player
+  // Enemies
+  // UI [last so it's on top]
+  // Pause or Game Over UI [so it's on top Score UI]
 
   // BUTTONS: //
   if (ButtonA.buttonState == LOW && ButtonA.buttonDebounce == false) {
     //ButtonA.buttonDebounce = true; 
     tft.fillCircle(SCREEN_WIDTH - (CircleA.radius * 1.5), SCREEN_HEIGHT - (CircleA.radius * 1.5) - 60, CircleA.radius, RED);
+    //Player.score -= 1;
   }
   else { // i need a stopgap / Debounce for this
     //ButtonA.buttonDebounce = false; 
@@ -220,6 +244,7 @@ void Render(float deltaTime) { // draw the graphics based on the Update changes
   if (ButtonB.buttonState == LOW && ButtonB.buttonDebounce == false) {
     //ButtonB.buttonDebounce = true; 
     tft.fillCircle(SCREEN_WIDTH - (CircleB.radius * 1.5), SCREEN_HEIGHT - (CircleB.radius * 1.5), CircleB.radius, GREEN);
+    //Player.score += 1;
   }
   else { // i need a stopgap / Debounce for this
     //ButtonB.buttonDebounce = false; 
@@ -234,6 +259,15 @@ void Render(float deltaTime) { // draw the graphics based on the Update changes
     //joystick.buttonDebounce = false;
     tft.fillCircle(Circle_JoystickButton.radius * 1.5, SCREEN_HEIGHT - (Circle_JoystickButton.radius * 1.5), Circle_JoystickButton.radius, BLACK);
   }
+
+
+  // UI Score:
+  tft.setTextColor(WHITE);
+  tft.setTextSize(3);
+  //String scoreStr = ("Score: " + String(Player.score)); // DON'T DO THIS EVERY FRAME! allocate a char[16]. 16 can be any number. What do would use then in games idk.
+  //const char* c_Based_ScoreStr = scoreStr.c_str(); 
+  printWrappedText("Score: 0", ((SCREEN_WIDTH / SCREEN_WIDTH) + 5) , ((SCREEN_HEIGHT / SCREEN_HEIGHT) + 5) , 16, 1);
+  // 00000 //
 }
 
 
